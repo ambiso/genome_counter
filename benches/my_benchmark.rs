e7425aa251374cfe40737a7183f8e70fbc389ca6
ambiso <ambiso@invalid>
2020-06-12 17:32:47 +0200

    Add 100k test

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -6,11 +6,17 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("count4", |b| b.iter(|| count_acgt("ACGT".as_bytes())));
     let mut rng = rand::thread_rng();
     let mut genome = String::new();
+    for _ in 0..100_000 {
+        genome.push("ACGT".chars().choose(&mut rng).unwrap());
+    }
+    println!("length: {}", genome.len());
+    c.bench_function("count_100k_rand", |b| b.iter(|| count_acgt(genome.as_bytes())));
+    let mut genome = String::new();
     for _ in 0..100_000_000 {
         genome.push("ACGT".chars().choose(&mut rng).unwrap());
     }
     println!("length: {}", genome.len());
-    c.bench_function("count_rand", |b| b.iter(|| count_acgt(genome.as_bytes())));
+    c.bench_function("count_100m_rand", |b| b.iter(|| count_acgt(genome.as_bytes())));
 }
 
 criterion_group!(benches, criterion_benchmark);
