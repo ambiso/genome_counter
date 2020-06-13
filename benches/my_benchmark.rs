d7243d358ed54c0aa1fb50bd156b3f8f94a7b6da
ambiso <ambiso@invalid>
2020-06-13 20:10:48 +0200

    Parallel

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,9 +1,9 @@
-use counter::count_acgt;
+use counter::count_opt;
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::seq::IteratorRandom;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("count4", |b| b.iter(|| count_acgt("ACGT".as_bytes())));
+    c.bench_function("count4", |b| b.iter(|| count_opt("ACGT".as_bytes())));
     let mut rng = rand::thread_rng();
     let mut genome = String::new();
     for _ in 0..100_000 {
@@ -11,7 +11,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
     println!("length: {}", genome.len());
     c.bench_function("count_100k_rand", |b| {
-        b.iter(|| count_acgt(genome.as_bytes()))
+        b.iter(|| count_opt(genome.as_bytes()))
     });
     let mut genome = String::new();
     for _ in 0..100_000_000 {
@@ -19,7 +19,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     }
     println!("length: {}", genome.len());
     c.bench_function("count_100m_rand", |b| {
-        b.iter(|| count_acgt(genome.as_bytes()))
+        b.iter(|| count_opt(genome.as_bytes()))
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
 use packed_simd::*;
 use std::collections::HashMap;
+use rayon::prelude::*;
 
 pub struct CounterResults {
-    a: u64,
-    c: u64,
-    g: u64,
-    t: u64,
+    pub a: u64,
+    pub c: u64,
+    pub g: u64,
+    pub t: u64,
 }
 
 impl CounterResults {
-    pub fn new() -> Self {
+    fn new() -> Self {
         Self {
             a: 0,
             c: 0,
@@ -17,50 +18,88 @@ impl CounterResults {
             t: 0,
         }
     }
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            a: self.a + other.a,
+            c: self.c + other.c,
+            g: self.g + other.g,
+            t: self.t + other.t,
+        }
+    }
 }
 
-pub fn count_acgt(s: &[u8]) -> Option<HashMap<char, u64>> {
+#[inline]
+fn check_results(s: &[u8], results: CounterResults) -> Option<CounterResults> {
+    if results.a + results.c + results.g + results.t != s.len() as u64 {
+        None
+    } else {
+        Some(results)
+    }
+}
+
+fn count_simple(s: &[u8]) -> CounterResults {
+    let mut results = CounterResults::new();
+    for v in s {
+        let v = *v as char;
+        results.a += (v == 'A') as u64;
+        results.c += (v == 'C') as u64;
+        results.g += (v == 'G') as u64;
+        results.t += (v == 'T') as u64;
+    }
+    results
+}
+
+pub fn count(s: &[u8]) -> Option<CounterResults> {
+    check_results(s, count_simple(s))
+}
+
+#[inline]
+fn count_letter(chunk: &u8x32, letter: &u8x32) -> u64 {
+    let a_eq = (*letter).eq(*chunk);
+    let count_a = u8x32::from_cast(a_eq).wrapping_sum();
+    (-(count_a as i8)) as u64
+}
+
+pub fn count_opt(s: &[u8]) -> Option<CounterResults> {
     let av = u8x32::splat('A' as u8);
     let cv = u8x32::splat('C' as u8);
     let gv = u8x32::splat('G' as u8);
     let tv = u8x32::splat('T' as u8);
 
-    #[inline]
-    fn count_letter(chunk: &u8x32, letter: &u8x32) -> u64 {
-        let a_eq = (*letter).eq(*chunk);
-        let count_a = u8x32::from_cast(a_eq).wrapping_sum();
-        (-(count_a as i8)) as u64
-    }
-
-    let mut results = CounterResults::new();
-    s.chunks_exact(32)
-        .map(u8x32::from_slice_unaligned)
-        .for_each(|chunk| {
-            results.a += count_letter(&chunk, &av);
-            results.c += count_letter(&chunk, &cv);
-            results.g += count_letter(&chunk, &gv);
-            results.t += count_letter(&chunk, &tv);
-        });
-    if results.a + results.c + results.g + results.t != s.len() as u64 {
-        None
-    } else {
-        let mut map = HashMap::with_capacity(4);
-        map.insert('A', results.a);
-        map.insert('C', results.c);
-        map.insert('G', results.g);
-        map.insert('T', results.t);
-        Some(map)
-    }
+    let results = s.par_chunks(32)
+        .map(|chunk| {
+            if chunk.len() >= 16 {
+                let chunk = u8x32::from_slice_unaligned(chunk);
+                CounterResults {
+                    a: count_letter(&chunk, &av),
+                    c: count_letter(&chunk, &cv),
+                    g: count_letter(&chunk, &gv),
+                    t: count_letter(&chunk, &tv),
+                }
+            } else {
+                count_simple(chunk)
+            }
+        })
+        .reduce(|| CounterResults::new(), CounterResults::add);
+    check_results(s, results)
 }
 
 #[cfg(test)]
 mod tests {
+    use rand::seq::IteratorRandom;
     #[test]
     fn it_works() {
-        let map = crate::count_acgt("AAAACCCCGGGGTTTTAAAAAAAACCTTTTTT".as_bytes()).unwrap();
-        assert_eq!(*map.get(&'A').unwrap(), 12);
-        assert_eq!(*map.get(&'C').unwrap(), 6);
-        assert_eq!(*map.get(&'G').unwrap(), 4);
-        assert_eq!(*map.get(&'T').unwrap(), 10);
+        let mut rng = rand::thread_rng();
+        let mut genome = String::new();
+        for _ in 0..1_000 {
+            genome.push("ACGT".chars().choose(&mut rng).unwrap());
+        }
+        let r1 = crate::count_opt(genome.as_bytes()).unwrap();
+        let r2 = crate::count(genome.as_bytes()).unwrap();
+        assert_eq!(r1.a, r2.a);
+        assert_eq!(r1.c, r2.c);
+        assert_eq!(r1.g, r2.g);
+        assert_eq!(r1.t, r2.t);
     }
 }
